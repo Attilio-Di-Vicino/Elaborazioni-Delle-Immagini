f56ce61e5fc5157bcae2b3a0d0e8fb2298acd86b
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-08-03 10:57:56 +0200

    canny

--- src/Algorithm/MyCanny.h
+++ src/Algorithm/MyCanny.h
@@ -16,97 +16,6 @@ class MyCanny {
 };
 
 /**
- * L'obiettivo della non-maximum suppression è quello di affinare i bordi dell'immagine,
- * mantenendo solo i punti che rappresentano i bordi veri e propri.
- * Questo viene fatto considerando l'orientamento del gradiente (fase) in ogni punto
- * e confrontando la magnitudo con i suoi vicini lungo questa direzione.
- * 
- * @param magnitude immagine di input
- * @param phase immagine di input
- * @param edges immagine di output
- */
-void MyCanny::nonMaxSuppression( Mat& magnitude, const Mat& phase, Mat& edges ) {
-
-    // Inizializzazione
-    edges = Mat::zeros( magnitude.size(), CV_8UC1 );
-    magnitude.copyTo( edges );
-
-    uchar q,r;
-    for ( int i = 0; i < magnitude.rows; i++ ) {
-        for ( int j = 0; j < magnitude.cols; j++ ) {
-
-            float angle = phase.at<float>( i, j ) > 180 ? phase.at<float>( i, j ) - 360: phase.at<float>( i, j );
-            uchar mag = magnitude.at<uchar>( i, j );
-
-            // Horizontal edge 0 gradi
-            if ( ( angle <= -157.5 && angle > 157.5 ) || ( angle > -22.5 && angle <= 22.5 ) ) { 
-				q = magnitude.at<uchar>( i, j-1 );
-                r = magnitude.at<uchar>( i, j+1 );
-			// -45 gradi
-            } else if ( ( angle <= -112.5 && angle > -157.5 ) || ( angle > 22.5 && angle <= 67.5 ) ) {
-                q = magnitude.at<uchar>( i-1, j-1 );
-                r = magnitude.at<uchar>( i+1, j+1 );
-			// Vertical edge 90 gradi
-            } else if ( ( angle <= -67.5 && angle > -112.5 ) || ( angle > 67.5 && angle <= 112.5 ) ) {
-                q = magnitude.at<uchar>( i+1, j );
-                r = magnitude.at<uchar>( i-1, j );	
-            // +45 gradi
-            } else if ( ( angle <= -22.5 && angle > -67.5 ) || ( angle > 112.5 && angle <= 157.5 ) ) {
-                q = magnitude.at<uchar>( i-1, j+1 );
-                r = magnitude.at<uchar>( i+1, j-1 );
-			}
-
-            if ( mag < q || mag < r ){
-                edges.at<uchar>( i, j ) = 0;
-                magnitude.at<uchar>( i, j ) = 0;
-            }
-        }
-    }
-}
-
-
-/**
- * La funzione thresholdingIsteresi esegue il thresholding con isteresi.
- * Divide i bordi in tre categorie: forti, deboli e non bordi.
- * Viene applicato un threshold inferiore (threshold1) per convertire i pixel con valori inferiori in pixel non bordo (0).
- * Viene applicato anche un threshold superiore (threshold2) per convertire i pixel con valori superiori in pixel bordo forti (255).
- * Sui pixel con valori compresi tra i due threshold viene eseguito un processo di connessione 
- * dei pixel deboli ai pixel forti adiacenti per determinare se vengono accettati come bordi o meno.
- * 
- * @param edges immagine di input e output
- * @param threshold1 parametro di sogliatura inferiore
- * @param threshold2 parametro di sogliatura superiore
-*/
-void MyCanny::thresholdingIsteresi( Mat& edges, double threshold1, double threshold2 ) {
-
-    for ( int i = 1; i < edges.rows - 1; i++ ) {
-        for ( int j = 1; j < edges.cols - 1; j++ ) {
-
-            uchar pixel = edges.at<uchar>( i, j );
-
-            if ( pixel > threshold2 )
-                pixel = 255;
-            else if ( pixel <= threshold1 )
-                pixel = 0;
-            else {
-                bool strongNeighbor = false;
-                for ( int x = -1; x <= 1 && !strongNeighbor; x++ ) {
-                    for ( int y = -1; y <= 1 && !strongNeighbor; y++ ) {
-                        if ( edges.at<uchar>( i + x, j + y ) > threshold2 )
-                            strongNeighbor = true;
-                    }
-                }
-                if ( strongNeighbor )
-                    pixel = 255;
-                else
-                    pixel = 0;
-            }
-            edges.at<uchar>( i, j ) = pixel;
-        }
-    }
-}
-
-/**
  * Canny è ampiamente utilizzato nel campo della visione artificiale
  * e dell'elaborazione delle immagini per individuare i bordi nelle immagini
  * 
@@ -147,8 +56,8 @@ void MyCanny::myCanny( const Mat& image, Mat& edges, double threshold1, double t
     // 1. Convolvere l'immagine di input con un filtro Gaussiano
     Mat paddedImage;
     Mat gaussianBlur;
-    copyMakeBorder( image, paddedImage, 1, 1, 1, 1, BORDER_REFLECT );
-    GaussianBlur( paddedImage, gaussianBlur, Size( 3, 3 ), 0, 0 );
+    // copyMakeBorder( image, paddedImage, 1, 1, 1, 1, BORDER_REFLECT );
+    GaussianBlur( paddedImage, gaussianBlur, Size( 5, 5 ), 0, 0 );
 
     // 2. Calcolare la magnitudo e l'orientazione del gradiente
     Mat dx, dy;
@@ -167,10 +76,10 @@ void MyCanny::myCanny( const Mat& image, Mat& edges, double threshold1, double t
     cv::magnitude( dx, dy, magnitude );
     magnitude.convertTo( magnitude, CV_8UC1 );
 
-    Sobel( gaussianBlur, dx, gaussianBlur.type(), 1, 0, apertureSize );
-    Sobel( gaussianBlur, dy, gaussianBlur.type(), 0, 1, apertureSize );
-    dx.convertTo( dx, CV_32FC1 );
-    dy.convertTo( dy, CV_32FC1 );
+    // Sobel( gaussianBlur, dx, gaussianBlur.type(), 1, 0, apertureSize );
+    // Sobel( gaussianBlur, dy, gaussianBlur.type(), 0, 1, apertureSize );
+    // dx.convertTo( dx, CV_32FC1 );
+    // dy.convertTo( dy, CV_32FC1 );
 
     /**
      * La Fase fornisce informazioni sull'orientamento del gradiente in ciascun punto dell'immagine.
@@ -219,4 +128,95 @@ void MyCanny::myCanny( const Mat& image, Mat& edges, double threshold1, double t
      * dei pixel deboli ai pixel forti adiacenti per determinare se vengono accettati come bordi o meno.
     */
     thresholdingIsteresi( edges, threshold1, threshold2 );
+}
+
+/**
+ * L'obiettivo della non-maximum suppression è quello di affinare i bordi dell'immagine,
+ * mantenendo solo i punti che rappresentano i bordi veri e propri.
+ * Questo viene fatto considerando l'orientamento del gradiente (fase) in ogni punto
+ * e confrontando la magnitudo con i suoi vicini lungo questa direzione.
+ * 
+ * @param magnitude immagine di input
+ * @param phase immagine di input
+ * @param edges immagine di output
+ */
+void MyCanny::nonMaxSuppression( Mat& magnitude, const Mat& phase, Mat& edges ) {
+
+    // Inizializzazione
+    edges = Mat::zeros( magnitude.size(), CV_8UC1 );
+    magnitude.copyTo( edges );
+
+    uchar q,r;
+    for ( int i = 0; i < magnitude.rows; i++ ) {
+        for ( int j = 0; j < magnitude.cols; j++ ) {
+
+            float angle = phase.at<float>( i, j ) > 180 ? phase.at<float>( i, j ) - 360: phase.at<float>( i, j );
+            uchar mag = magnitude.at<uchar>( i, j );
+
+            // Horizontal edge 0 gradi
+            if ( ( angle <= -157.5 && angle > 157.5 ) || ( angle > -22.5 && angle <= 22.5 ) ) { 
+				q = magnitude.at<uchar>( i, j-1 );
+                r = magnitude.at<uchar>( i, j+1 );
+			// -45 gradi
+            } else if ( ( angle <= -112.5 && angle > -157.5 ) || ( angle > 22.5 && angle <= 67.5 ) ) {
+                q = magnitude.at<uchar>( i-1, j-1 );
+                r = magnitude.at<uchar>( i+1, j+1 );
+			// Vertical edge 90 gradi
+            } else if ( ( angle <= -67.5 && angle > -112.5 ) || ( angle > 67.5 && angle <= 112.5 ) ) {
+                q = magnitude.at<uchar>( i+1, j );
+                r = magnitude.at<uchar>( i-1, j );	
+            // +45 gradi
+            } else if ( ( angle <= -22.5 && angle > -67.5 ) || ( angle > 112.5 && angle <= 157.5 ) ) {
+                q = magnitude.at<uchar>( i-1, j+1 );
+                r = magnitude.at<uchar>( i+1, j-1 );
+	    } 
+	
+            if ( mag < q || mag < r ){
+                edges.at<uchar>( i, j ) = 0;
+                magnitude.at<uchar>( i, j ) = 0;
+            }
+        }
+    }
+}
+
+
+/**
+ * La funzione thresholdingIsteresi esegue il thresholding con isteresi.
+ * Divide i bordi in tre categorie: forti, deboli e non bordi.
+ * Viene applicato un threshold inferiore (threshold1) per convertire i pixel con valori inferiori in pixel non bordo (0).
+ * Viene applicato anche un threshold superiore (threshold2) per convertire i pixel con valori superiori in pixel bordo forti (255).
+ * Sui pixel con valori compresi tra i due threshold viene eseguito un processo di connessione 
+ * dei pixel deboli ai pixel forti adiacenti per determinare se vengono accettati come bordi o meno.
+ * 
+ * @param edges immagine di input e output
+ * @param threshold1 parametro di sogliatura inferiore
+ * @param threshold2 parametro di sogliatura superiore
+*/
+void MyCanny::thresholdingIsteresi( Mat& edges, double threshold1, double threshold2 ) {
+
+    for ( int i = 1; i < edges.rows - 1; i++ ) {
+        for ( int j = 1; j < edges.cols - 1; j++ ) {
+
+            uchar pixel = edges.at<uchar>( i, j );
+
+            if ( pixel > threshold2 )
+                pixel = 255;
+            else if ( pixel <= threshold1 )
+                pixel = 0;
+            else {
+                bool strongNeighbor = false;
+                for ( int x = -1; x <= 1 && !strongNeighbor; x++ ) {
+                    for ( int y = -1; y <= 1 && !strongNeighbor; y++ ) {
+                        if ( edges.at<uchar>( i + x, j + y ) > threshold2 )
+                            strongNeighbor = true;
+                    }
+                }
+                if ( strongNeighbor )
+                    pixel = 255;
+                else
+                    pixel = 0;
+            }
+            edges.at<uchar>( i, j ) = pixel;
+        }
+    }
 }
